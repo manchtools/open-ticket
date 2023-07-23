1de3e8818cf2c3aa341c6b8d7be7a48f15371baf
Paul <paul.dotterer@icloud.com>
2023-07-23 18:05:30 +0200

    removed console.log

--- src/routes/admin/tickets/+page.server.js
+++ src/routes/admin/tickets/+page.server.js
@@ -2,6 +2,6 @@ import { AppwriteNodeAgentService } from '$lib/AppwriteNodeService';
 
 export async function load() {
 	const tickets = await AppwriteNodeAgentService.getOpenTickets();
-	console.log(tickets);
+
 	return { tickets };
 }
