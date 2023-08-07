a6af70b34890293544341fadb936ecf461cae1c5
Paul <paul.dotterer@icloud.com>
2023-08-07 16:37:07 +0200

    horizontal divider only shows if auth providers exist

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -24,19 +24,21 @@
 			<input type="password" name="password" class="input" />
 		</label>
 		<button class="btn variant-ghost-primary w-fit self-center">Login</button>
-		<hr class="my-4" />
-		<div class="flex gap-4 flex-col w-[95%] md:w-1/3 self-center">
-			{#each data.authMethods.authProviders as provider}
-				<button
-					class="btn variant-filled flex flex-row"
-					formaction="?/loginOauth&provider={provider.name}"
-				>
-					<i class="fa-brands fa-{provider.name}" />
-					<div class="flex">
-						<p>Sign in with {provider.name}</p>
-					</div>
-				</button>
-			{/each}
-		</div>
+		{#if data.authMethods.authProviders.length > 0}
+			<hr class="my-4" />
+			<div class="flex gap-4 flex-col w-[95%] md:w-1/3 self-center">
+				{#each data.authMethods.authProviders as provider}
+					<button
+						class="btn variant-filled flex flex-row"
+						formaction="?/loginOauth&provider={provider.name}"
+					>
+						<i class="fa-brands fa-{provider.name}" />
+						<div class="flex">
+							<p>Sign in with {provider.name}</p>
+						</div>
+					</button>
+				{/each}
+			</div>
+		{/if}
 	</form>
 </div>
