004a2d8ba8e08a58944c92974cfc97a7898bf828
Paul <paul.dotterer@icloud.com>
2023-08-08 10:46:36 +0200

    combined seperate hook functions

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,47 +1,56 @@
-onRecordAfterUpdateRequest((e) => {
-	let tmpRecord = JSON.parse(JSON.stringify(e.record));
-	const collection = $app.dao().findCollectionByNameOrId('tickets_versions');
-	tmpRecord['ticket'] = e.record.id;
-	delete tmpRecord.id;
-	delete tmpRecord.created;
-	delete tmpRecord.updated;
-	const record = new Record(collection, tmpRecord);
-	$app.dao().saveRecord(record);
-}, 'tickets');
+onRecordAfterUpdateRequest(
+	(e) => {
+		const info = $apis.requestInfo(e.httpContext);
+		let tmpRecord = JSON.parse(JSON.stringify(e.record));
+		let collection;
 
-onRecordAfterCreateRequest((e) => {
-	let tmpRecord = JSON.parse(JSON.stringify(e.record));
-	const collection = $app.dao().findCollectionByNameOrId('tickets_versions');
-	tmpRecord['ticket'] = e.record.id;
-	delete tmpRecord.id;
-	delete tmpRecord.created;
-	delete tmpRecord.updated;
-	const record = new Record(collection, tmpRecord);
-	$app.dao().saveRecord(record);
-}, 'tickets');
+		if (e.collection.name === 'tickets') {
+			collection = $app.dao().findCollectionByNameOrId('tickets_versions');
+			tmpRecord['ticket'] = e.record.id;
+		}
+		if (e.collection.name === 'replies') {
+			collection = $app.dao().findCollectionByNameOrId('replies_versions');
+			tmpRecord['reply'] = e.record.id;
+		}
 
-onRecordAfterUpdateRequest((e) => {
-	let tmpRecord = JSON.parse(JSON.stringify(e.record));
-	const collection = $app.dao().findCollectionByNameOrId('replies_versions');
-	tmpRecord['ticket'] = e.record.id;
-	delete tmpRecord.id;
-	delete tmpRecord.created;
-	delete tmpRecord.updated;
-	const record = new Record(collection, tmpRecord);
-	$app.dao().saveRecord(record);
-}, 'replies');
+		if (info.authRecord) {
+			tmpRecord['updatedBy'] = info.authRecord.id;
+		}
+		delete tmpRecord.id;
+		delete tmpRecord.created;
+		delete tmpRecord.updated;
+		const record = new Record(collection, tmpRecord);
+		$app.dao().saveRecord(record);
+	},
+	'tickets',
+	'replies'
+);
 
-onRecordAfterCreateRequest((e) => {
-	let tmpRecord = JSON.parse(JSON.stringify(e.record));
-	const collection = $app.dao().findCollectionByNameOrId('replies_versions');
-	tmpRecord['reply'] = e.record.id;
-	delete tmpRecord.id;
-	delete tmpRecord.created;
-	delete tmpRecord.updated;
-	const record = new Record(collection, tmpRecord);
-	$app.dao().saveRecord(record);
-}, 'replies');
-
+onRecordAfterCreateRequest(
+	(e) => {
+		const info = $apis.requestInfo(e.httpContext);
+		let tmpRecord = JSON.parse(JSON.stringify(e.record));
+		let collection;
+		if (e.collection.name === 'tickets') {
+			collection = $app.dao().findCollectionByNameOrId('tickets_versions');
+			tmpRecord['ticket'] = e.record.id;
+		}
+		if (e.collection.name === 'replies') {
+			collection = $app.dao().findCollectionByNameOrId('replies_versions');
+			tmpRecord['reply'] = e.record.id;
+		}
+		if (info.authRecord) {
+			tmpRecord['updatedBy'] = info.authRecord.id;
+		}
+		delete tmpRecord.id;
+		delete tmpRecord.created;
+		delete tmpRecord.updated;
+		const record = new Record(collection, tmpRecord);
+		$app.dao().saveRecord(record);
+	},
+	'tickets',
+	'replies'
+);
 onRecordBeforeUpdateRequest(
 	(e) => {
 		throw new BadRequestError("Can't update version records");
