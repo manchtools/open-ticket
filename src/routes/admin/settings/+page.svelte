91fdb6281a80e6b4774d1b6c268fb200de19c6c1
Paul <paul.dotterer@icloud.com>
2023-08-12 17:02:43 +0200

    Added headings and Auth Provider card

--- src/routes/admin/settings/+page.svelte
+++ src/routes/admin/settings/+page.svelte
@@ -1,16 +1,30 @@
-<div class="flex gap-4 flex-wrap mb-4">
-	<a
-		href="/admin/settings/users"
-		class="card card-hover p-4 w-fit h-fit items-center flex flex-col gap-4"
-	>
-		<i class="fa-solid fa-users card-header fa-xl" />
-		<section>Users</section>
-	</a>
-	<a
-		href="/admin/settings/admins"
-		class="card card-hover p-4 w-fit h-fit items-center flex flex-col gap-4"
-	>
-		<i class="fa-solid fa-users-gear card-header fa-xl" />
-		<section>Admins</section>
-	</a>
+<div class="flex flex-col gap-2 flex-wrap mb-4">
+	<h1>User Management</h1>
+	<div class="flex gap-4">
+		<a
+			href="/admin/settings/users"
+			class="card card-hover p-4 w-fit h-fit items-center flex flex-col gap-4"
+		>
+			<i class="fa-solid fa-users card-header fa-xl" />
+			<section>Users</section>
+		</a>
+		<a
+			href="/admin/settings/admins"
+			class="card card-hover p-4 w-fit h-fit items-center flex flex-col gap-4"
+		>
+			<i class="fa-solid fa-users-gear card-header fa-xl" />
+			<section>Admins</section>
+		</a>
+	</div>
+	<hr />
+	<h1>System Management</h1>
+	<div class="flex gap-4">
+		<a
+			href="/admin/settings/system/auth-provider"
+			class="card card-hover p-4 w-fit h-fit items-center flex flex-col gap-4"
+		>
+			<i class="fa-solid fa-key fa-xl card-header" />
+			<section>Auth Provider</section>
+		</a>
+	</div>
 </div>
