932e7b1e56150024fccae019c3fa54e08ea01076
Paul <paul.dotterer@icloud.com>
2023-08-12 17:42:02 +0200

    added realtime subsctiption for change events

--- src/lib/tickets/Ticket.svelte
+++ src/lib/tickets/Ticket.svelte
@@ -1,8 +1,45 @@
 <script>
 	import TicketBody from './TicketBody.svelte';
 	import TicketHead from './TicketHead.svelte';
+	import { toastStore } from '@skeletonlabs/skeleton';
+	import { onDestroy, onMount } from 'svelte';
+	import { pb } from '$lib/db';
 
 	export let data;
+	onMount(() => {
+		pb.collection('tickets').subscribe(data.id, async (e) => {
+			let tmp = await pb
+				.collection('tickets')
+				.getOne(e.record.id, { expand: 'replies,replies.createdBy,agent,createdBy,updatedBy' });
+
+			let message = '';
+			if (tmp.replies.length !== data.replies.length) {
+				message = message + 'replied to this ticket';
+			}
+			if (tmp.status !== data.status) {
+				if (message.length >= 1) {
+					message += ', and ';
+				}
+				message += `updated status to <span class="underline">${tmp.status}</span>`;
+			}
+			if (tmp.agent != data.agent) {
+				if (message.length >= 1) {
+					message += ', and ';
+				}
+				message += `updated agent to <span class="underline">${tmp.expand.agent.email}</span>`;
+			}
+			data = tmp;
+			if (e.action === 'update' && e.record?.updatedBy !== pb.authStore.model.id) {
+				toastStore.trigger({
+					message: `<b class="underline">${tmp.expand.updatedBy.email}</b> ${message}.`,
+					background: 'variant-ghost-warning'
+				});
+			}
+		});
+	});
+	onDestroy(() => {
+		pb.collection('tickets').unsubscribe(data.id);
+	});
 </script>
 
 <div class="card p-4">
