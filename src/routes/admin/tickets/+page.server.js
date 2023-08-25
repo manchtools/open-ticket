aab17bb0af54f5e259f73d5a55f14f76395c911c
Paul <paul.dotterer@icloud.com>
2023-08-25 19:42:57 +0200

    updated filter to propperly handle search and queue

--- src/routes/admin/tickets/+page.server.js
+++ src/routes/admin/tickets/+page.server.js
@@ -4,24 +4,30 @@ export async function load({ url, locals }) {
 	const offset = parseInt(url.searchParams.get('offset')) || 1;
 	const searchTerm = url.searchParams.get('search');
 	const queue = url.searchParams.get('queue');
-
+	console.log(searchTerm, queue);
 	let searchData = {
 		sort: '-created',
 		expand: 'createdBy,agent,queue',
 		filter: ''
 	};
-	if (searchTerm) {
+	if (searchTerm && queue) {
 		searchData[
 			'filter'
-		] = `body ~ '${searchTerm}'||subject ~ '${searchTerm}'||id = '${searchTerm}'||agent.name ~ '${searchTerm}'||agent.email ~ '${searchTerm}'||createdBy.name ~ '${searchTerm}'||createdBy.email ~ '${searchTerm}'`;
-	}
-	if (searchTerm & queue) {
-		searchData['filter'] += `&&`;
-	}
-	if (queue) {
-		searchData['filter'] += `queue.id = '${queue}'`;
+		] = `(body ~ '${searchTerm}'||subject ~ '${searchTerm}'||id = '${searchTerm}'||agent.name ~ '${searchTerm}'||agent.email ~ '${searchTerm}'||createdBy.name ~ '${searchTerm}'||createdBy.email ~ '${searchTerm}')&&queue.id = '${queue}'`;
+	} else {
+		if (searchTerm) {
+			searchData[
+				'filter'
+			] = `body ~ '${searchTerm}'||subject ~ '${searchTerm}'||id = '${searchTerm}'||agent.name ~ '${searchTerm}'||agent.email ~ '${searchTerm}'||createdBy.name ~ '${searchTerm}'||createdBy.email ~ '${searchTerm}'`;
+		}
+
+		if (queue) {
+			searchData['filter'] = `queue.id = '${queue}'`;
+		}
 	}
 	console.log(searchData);
-	const tickets = await locals.pb.collection('tickets').getList(offset, 25, searchData);
-	return { tickets: serializePoJos(tickets) };
+	try {
+		const tickets = await locals.pb.collection('tickets').getList(offset, 25, searchData);
+		return { tickets: serializePoJos(tickets) };
+	} catch (e) {}
 }
