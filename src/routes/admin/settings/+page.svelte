acbe6ea9ca18cfaea8275ba3bac18437e4f5eef8
Paul <paul.dotterer@icloud.com>
2023-08-25 14:30:43 +0200

    added shortcut to queue management and added lucide-svelte

--- src/routes/admin/settings/+page.svelte
+++ src/routes/admin/settings/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { ListOrdered, KeyRound } from 'lucide-svelte';
+</script>
+
 <div class="flex flex-col gap-2 flex-wrap mb-4">
 	<h1>User Management</h1>
 	<div class="flex gap-4">
@@ -17,13 +21,24 @@
 		</a>
 	</div>
 	<hr />
+	<h1>Ticket Management</h1>
+	<div class="flex gap-4">
+		<a
+			href="/admin/settings/tickets/queues"
+			class="card card-hover p-4 w-fit h-fit items-center flex flex-col"
+		>
+			<ListOrdered size="36" strokeWidth="1.5" />
+			<section>Queues</section>
+		</a>
+	</div>
+	<hr />
 	<h1>System Management</h1>
 	<div class="flex gap-4">
 		<a
 			href="/admin/settings/system/auth-provider"
-			class="card card-hover p-4 w-fit h-fit items-center flex flex-col gap-4"
+			class="card card-hover p-4 w-fit h-fit items-center flex flex-col"
 		>
-			<i class="fa-solid fa-key fa-xl card-header" />
+			<KeyRound size="36" strokeWidth="1.5" />
 			<section>Auth Provider</section>
 		</a>
 	</div>
