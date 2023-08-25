d501d23285456af3ae37273dca958eee8eccaa1e
Paul <paul.dotterer@icloud.com>
2023-08-25 19:08:09 +0200

    adder filtering for queue

--- src/routes/admin/tickets/+page.server.js
+++ src/routes/admin/tickets/+page.server.js
@@ -3,6 +3,7 @@ import { serializePoJos } from '$lib/helpers';
 export async function load({ url, locals }) {
 	const offset = parseInt(url.searchParams.get('offset')) || 1;
 	const searchTerm = url.searchParams.get('search');
+	const queue = url.searchParams.get('queue');
 
 	let searchData = {
 		sort: '-created',
@@ -14,6 +15,13 @@ export async function load({ url, locals }) {
 			'filter'
 		] = `body ~ '${searchTerm}'||subject ~ '${searchTerm}'||id = '${searchTerm}'||agent.name ~ '${searchTerm}'||agent.email ~ '${searchTerm}'||createdBy.name ~ '${searchTerm}'||createdBy.email ~ '${searchTerm}'`;
 	}
+	if (searchTerm & queue) {
+		searchData['filter'] += `&&`;
+	}
+	if (queue) {
+		searchData['filter'] += `queue.id = '${queue}'`;
+	}
+	console.log(searchData);
 	const tickets = await locals.pb.collection('tickets').getList(offset, 25, searchData);
 	return { tickets: serializePoJos(tickets) };
 }
