419185dd20e045a6e67a2052dac30fef8c7e8075
Paul <paul.dotterer@icloud.com>
2023-09-01 13:14:50 +0200

    checkbox and email/name are now inline

--- src/lib/queue/CreateQueue.svelte
+++ src/lib/queue/CreateQueue.svelte
@@ -8,8 +8,12 @@
 	<input type="text" class="input" name="name" />
 	<input type="text" name="members" value={selectString} hidden />
 	{#each $page.data.agents as agent}
-		<input type="checkbox" class="checkbox" value={agent.id} bind:group={selection} />
-		{agent.name || agent.email}
+		<label class="flex items-center gap-2">
+			<input type="checkbox" class="checkbox" value={agent.id} bind:group={selection} />
+			<p>
+				{agent.name || agent.email}
+			</p>
+		</label>
 	{/each}
 
 	<button class="btn variant-ghost-success w-fit">Create</button>
