b2b37962dd98e4eec944fd82a093f2207312b79e
Paul <paul.dotterer@icloud.com>
2024-01-01 22:01:48 +0100

    moved storage bucket creation to createTicket function

--- functions/manageTicketStorageBucket/src/main.js
+++ functions/manageTicketStorageBucket/src/main.js
@@ -1,28 +1,15 @@
-import { Client, Storage,Databases, Permission,Role} from 'node-appwrite';
+import { Client, Storage } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
-
   const client = new Client()
     .setEndpoint(process.env.ENDPOINT)
     .setProject(process.env.PROJECT)
-    .setKey(process.env.API_KEY).setSelfSigned();
+    .setKey(process.env.API_KEY)
+    .setSelfSigned();
 
-  const storage = new Storage(client)
-  const databases = new Databases(client)
-  log(req)
- if(req.headers["x-appwrite-event"].includes(".create")){
-  const ticket = await databases.getDocument("main","tickets",req.body.$id)
-  await storage.createBucket(req.body.$id,req.body.$id,[
-    Permission.write(Role.label("agent")),
-    Permission.read(Role.label("agent")),
-    Permission.read(Role.user(req.headers["x-appwrite-user-id"])),
-    Permission.create(Role.user(req.headers["x-appwrite-user-id"]))
-  ])
- }
+  const storage = new Storage(client);
 
+  await storage.deleteBucket(req.body.$id, req.body.$id);
 
- if(req.headers["x-appwrite-event"].includes(".delete")){
-   await storage.deleteBucket(req.body.$id,req.body.$id)
- }
-  return res.empty()
-}
+  return res.empty();
+};
