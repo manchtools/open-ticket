7e457bbe370e894493414f6806bbe0e43d01c652
Paul <paul.dotterer@icloud.com>
2024-01-20 18:03:38 +0100

    added autoscroll to bottom

--- src/lib/components/ui/ticket/ticketReplies.svelte
+++ src/lib/components/ui/ticket/ticketReplies.svelte
@@ -4,15 +4,35 @@
 	import * as Form from '$lib/components/ui/form';
 	import { replySchema } from './ticketSchema';
 	import { page } from '$app/stores';
+	import { afterUpdate, onMount } from 'svelte';
+
 	export let ticket;
 	export let form;
+	function scrollToLastMessage() {
+		document.getElementById('anchor')?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+	}
+	onMount(() => {
+		scrollToLastMessage();
+	});
+	afterUpdate(() => {
+		scrollToLastMessage();
+	});
 </script>
 
 <Card.Root class="flex w-full flex-col border-none">
+	<Card.Header>
+		<Card.Description class="self-end"
+			>{ticket.expand['replies(ticket)']?.length || 0} Replies</Card.Description
+		>
+	</Card.Header>
 	<Card.Content class="flex max-h-[450px] flex-col gap-4 overflow-scroll p-4">
+		{#if !ticket?.expand['replies(ticket)']}
+			<h3 class="self-center">No messages, start a new conversation!</h3>
+		{/if}
 		{#each ticket?.expand['replies(ticket)'] || [] as reply}
 			<MessageBubble {reply}></MessageBubble>
 		{/each}
+		<div id="anchor"></div>
 	</Card.Content>
 	<hr />
 	<Card.Footer class="p-2 md:p-4">
@@ -23,28 +43,29 @@
 			schema={replySchema}
 			let:config
 			class="flex w-full gap-2"
-			options={{ resetForm: true }}
+			options={{
+				resetForm: true
+			}}
 		>
-			<div class="flex w-full flex-col">
-				<Form.Field {config} name="body">
-					<Form.Item class="w-full">
-						<Form.Textarea placeholder="Message"></Form.Textarea>
+			<Form.Field {config} name="body">
+				<Form.Item class="w-full">
+					<Form.Textarea placeholder="Message"></Form.Textarea>
 
-						<Form.Validation />
-					</Form.Item>
-				</Form.Field>
+					<Form.Validation />
+				</Form.Item>
+			</Form.Field>
+			<div class="flex flex-col gap-4">
+				<Form.Button>Send</Form.Button>
 				{#if $page.data.user.type.includes('agent')}
 					<Form.Field {config} name="private">
 						<Form.Item class="flex items-center gap-2  space-y-0 self-end">
 							<Form.Switch />
-							<Form.Label>Private reply</Form.Label>
+							<Form.Label class="text-nowrap">Private reply</Form.Label>
 							<Form.Validation />
 						</Form.Item>
 					</Form.Field>
 				{/if}
 			</div>
-
-			<Form.Button>Send</Form.Button>
 		</Form.Root>
 	</Card.Footer>
 </Card.Root>
